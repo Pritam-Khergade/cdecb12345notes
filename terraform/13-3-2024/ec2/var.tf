071d8ba86f15f042789f8cfcbc47f9c4431ea89e
Pritam Khergade <102211747+Pritam-Khergade@users.noreply.github.com>
2024-03-13 10:20:32 +0530

    Create var.tf

---
+++ terraform/13-3-2024/ec2/var.tf
@@ -0,0 +1,24 @@
+variable "ami" {
+
+}
+variable "instancetype" {
+
+}
+variable "subnet_id" {
+
+}
+variable "vpcid" {
+
+}
+variable "tag_name" {
+
+}
+variable "env" {
+
+}
+variable "keyname" {
+
+}
+variable "sg" {
+
+}
